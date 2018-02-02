1784c5ebb91bdd7dadc2fbdf97765611f3f34eee
Shantal Taveras <shantaltaveras@ac.c4q.nyc>
2018-02-01 20:15:01 -0500

    started working on detail activity, and layout

--- app/src/main/java/c4q/com/comic_vine/ComicDetail.java
+++ app/src/main/java/c4q/com/comic_vine/ComicDetail.java
@@ -1,10 +1,37 @@
 package c4q.com.comic_vine;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by Shant on 1/30/2018.
  */
 
 public class ComicDetail extends AppCompatActivity {
+
+    ImageView image;
+    TextView name;
+    TextView desc;
+    TextView pub;
+    TextView resType;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.comic_detail);
+
+        setReferences();
+
+    }
+
+    public void setReferences(){
+        image = findViewById(R.id.detail_image);
+        name = findViewById(R.id.detail_name);
+        desc = findViewById(R.id.detail_desc);
+        resType = findViewById(R.id.resource_type);
+        pub = findViewById(R.id.detail_publisher);
+    }
 }
