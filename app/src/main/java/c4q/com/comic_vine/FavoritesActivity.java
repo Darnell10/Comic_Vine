2e01113f98b377090677583aa97e2c52de585d80
Darnell O <DarnellOtto@ac.c4q.nyc>
2018-02-06 13:40:40 -0500

    Favorite activit and RV added

--- app/src/main/java/c4q/com/comic_vine/FavoritesActivity.java
+++ app/src/main/java/c4q/com/comic_vine/FavoritesActivity.java
@@ -2,9 +2,13 @@ package c4q.com.comic_vine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import c4q.com.comic_vine.controller.FavoritesAdapter;
 
 public class FavoritesActivity extends AppCompatActivity {
 
@@ -15,6 +19,14 @@ public class FavoritesActivity extends AppCompatActivity {
 
         ComicSQL comicSQL = ComicSQL.getInstance(getApplicationContext());
         List<Favorites> comicFaves = comicSQL.favorites();
+
+        RecyclerView favoriteRecyclerview = findViewById(R.id.favorite_rv);
+
+        FavoritesAdapter favoritesAdapter = new FavoritesAdapter(comicFaves);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+
+        favoriteRecyclerview.setAdapter(favoritesAdapter);
+        favoriteRecyclerview.setLayoutManager(linearLayoutManager);
 
     }
 }
